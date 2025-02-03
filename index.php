0766cf3eee425952b18eba2e2a2a09a2feebd777
Kellyan01 <depriester.yoann@neuf.fr>
2025-02-03 11:54:56 +0100

    first commit task cda

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+//include './vendor/autoload.php';
+include './env.php';
+include './utils/connexion.php';
+
+include 'controller/categorieController.php';
+$bdd = connexion();
+ajouterCategory($bdd);
+
